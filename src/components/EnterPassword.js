3174abfdda83f888fb755d0aaf716eccb61c67c3
Richard Charity <richardtcharityjr@gmail.com>
2023-02-22 16:45:43 -0500

    Displays Input

--- src/components/EnterPassword.js
+++ src/components/EnterPassword.js
@@ -1,31 +1,26 @@
-import React, {useState, useRef, Text} from 'react'
-import { logPassword, logPasswordUpper } from "../functions/backend";
-
+import React, { useState, useRef,  } from "react";
+import { logPasswordUpper } from "../functions/backend";
 
 export default function EnterPassword() {
 
-    const passwordRef = useRef() 
+  // React Hooks
+  const passwordRef = useRef();
+  const [ printPassword, setPrintPassword ] = useState("");
 
 
-
-    const {printPassword, setPrintPassword} = useState([])
-
-    function onSubmit(e) {
-       const password = passwordRef.current.value;
-       if (password === '') return
-       
-       logPassword(password)
-       printPassword = logPasswordUpper(password)
-    }
+  // On Submit Function
+  function onSubmit(e) {
+    const password = passwordRef.current.value;
+    if (password === "") return;
+    logPasswordUpper(password);
+    setPrintPassword(password);
+  }
 
   return (
     <>
-    <div>
-      <h1>{printPassword}</h1>
-    </div>
       <input ref={passwordRef} type="text" />
       <button onClick={onSubmit}>Submit</button>
-      
+      <h1>{printPassword}</h1>
     </>
   );
 }
--- src/functions/backend.js
+++ src/functions/backend.js
@@ -1,7 +1,9 @@
 export function logPassword(passwordToCheck) {
   console.log(passwordToCheck);
+  return passwordToCheck;
 }
 
 export function logPasswordUpper(passwordToCheck) {
   console.log(passwordToCheck.toUpperCase())
+  return passwordToCheck.toUpperCase();
 }
