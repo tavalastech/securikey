407e1fc9b1197bb55a72dbcefb52731304c18796
Richard Charity <richardtcharityjr@gmail.com>
2023-02-28 18:00:01 -0500

    Updated

--- src/components/EnterPassword.js
+++ src/components/EnterPassword.js
@@ -1,26 +1,35 @@
 import React, { useState, useRef,  } from "react";
-import { logPasswordUpper } from "../functions/backend";
+import { logPassword } from "../functions/backend";
 
 export default function EnterPassword() {
 
   // React Hooks
   const passwordRef = useRef();
   const [ printPassword, setPrintPassword ] = useState("");
+  const [isShown, setIsShown] = useState(false);
+
 
 
   // On Submit Function
   function onSubmit(e) {
     const password = passwordRef.current.value;
     if (password === "") return;
-    logPasswordUpper(password);
+    logPassword(password);
     setPrintPassword(password);
+    setIsShown((current) => !current);
   }
 
   return (
     <>
       <input ref={passwordRef} type="text" />
       <button onClick={onSubmit}>Submit</button>
-      <h1>{printPassword}</h1>
+
+      {/* 👇️ show elements on click */}
+      {isShown && (
+        <div>
+          <h1>This is the input: {printPassword}</h1>
+        </div>
+      )}
     </>
   );
 }
