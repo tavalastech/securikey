1df9440ccd3065ab1950249fad954329235f0c54
Tkistler12 <99052162+Tkistler12@users.noreply.github.com>
2023-03-01 17:27:18 -0500

    Output of test results

--- src/components/EnterPassword.js
+++ src/components/EnterPassword.js
@@ -1,12 +1,18 @@
 import React, { useState, useRef,  } from "react";
-import { logPassword } from "../functions/backend";
+import { testNumbers, testLowercase, testUppercase, testSpecial, testLength, testOverall  } from "../functions/backend";
 
 export default function EnterPassword() {
 
   // React Hooks
   const passwordRef = useRef();
-  const [ printPassword, setPrintPassword ] = useState("");
+  //const [ printPassword, setPrintPassword ] = useState("");
   const [isShown, setIsShown] = useState(false);
+  const [number, setNumber] = useState("");
+  const [lowercase, setLowercase] = useState(""); 
+  const [uppercase, setUppercase] = useState(""); 
+  const [special, setSpecial] = useState("");
+  const [length, setLength] = useState(""); 
+  const [overall, setOverall] = useState(""); 
 
 
 
@@ -14,9 +20,18 @@ export default function EnterPassword() {
   function onSubmit(e) {
     const password = passwordRef.current.value;
     if (password === "") return;
-    logPassword(password);
-    setPrintPassword(password);
+    //logPassword(password);
+    setNumber(testNumbers(password) + "");
+    setLowercase(testLowercase(password) + ""); 
+    setUppercase(testUppercase(password) + "");
+    setSpecial(testSpecial(password) + ""); 
+    setLength(testLength(password) + ""); 
+    setOverall(testOverall(password) + ""); 
+    //setPrintPassword(password);
+    //var t1 = testPassword(password);
+    //console.log (t1);
     setIsShown((current) => !current);
+    //settrying(t1 + "");
   }
 
   return (
@@ -27,7 +42,19 @@ export default function EnterPassword() {
       {/* 👇️ show elements on click */}
       {isShown && (
         <div>
-          <h1>This is the input: {printPassword}</h1>
+          {/* <h1>This is the input: {printPassword}</h1>
+          <p>This is the output: {trying}</p> */}
+          <h1>Password Strength Test Results</h1>
+          <section>
+            <div>Does it contain a number: {number}</div>
+            <div>Does it contain a lowercase letter: {lowercase}</div>
+            <div>Does it contain a uppercase letter: {uppercase}</div>
+            <div>Does it contain a special character: {special}</div>
+            <div>Is it atleast 8 characters long: {length}</div>
+          </section>
+          <h2>Does This Password Past The Test: {overall}</h2>
+
+          
         </div>
       )}
     </>
--- src/functions/backend.js
+++ src/functions/backend.js
@@ -1,17 +1,51 @@
-export function logPassword(passwordToCheck) {
-  const fullregex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!]).{8,}$/;
-  const numbers = /^(?=.*\d)/;
-  const lowercase = /^(?=.*[a-z])/;
-  const uppercase = /^(?=.*[A-Z])/;
-  const special = /^(?=.*["'()*,-./:;<>?_`~@#$%^&+=!\][}{|\\ ])/;
-  const length = /.{8,}$/;
+// export function logPassword(passwordToCheck) {
+//   const fullregex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!]).{8,}$/;
+//   const numbers = /^(?=.*\d)/;
+//   const lowercase = /^(?=.*[a-z])/;
+//   const uppercase = /^(?=.*[A-Z])/;
+//   const special = /^(?=.*["'()*,-./:;<>?_`~@#$%^&+=!\][}{|\\ ])/;
+//   const length = /.{8,}$/;
 
-  console.log("Numbers: ", numbers.test(passwordToCheck));
-  console.log("Lowercase: ", lowercase.test(passwordToCheck));
-  console.log("Uppercase: ", uppercase.test(passwordToCheck));
-  console.log("Special character: ", special.test(passwordToCheck));
-  console.log(">= 8: ", length.test(passwordToCheck));
-  console.log("Overall:", fullregex.test(passwordToCheck));
+//   console.log("Numbers: ", numbers.test(passwordToCheck));
+//   console.log("Lowercase: ", lowercase.test(passwordToCheck));
+//   console.log("Uppercase: ", uppercase.test(passwordToCheck));
+//   console.log("Special character: ", special.test(passwordToCheck));
+//   console.log(">= 8: ", length.test(passwordToCheck));
+//   console.log("Overall:", fullregex.test(passwordToCheck));
+// }
+
+const fullregex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!]).{8,}$/;
+const numbers = /^(?=.*\d)/;
+const lowercase = /^(?=.*[a-z])/;
+const uppercase = /^(?=.*[A-Z])/;
+const special = /^(?=.*["'()*,-./:;<>?_`~@#$%^&+=!\][}{|\\ ])/;
+const length = /.{8,}$/;
+
+// export function testPassword(passwordToCheck){
+//   const fullregex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!]).{8,}$/;
+//   return (fullregex.test(passwordToCheck));
+// }
+
+export function testNumbers (passwordToCheck) { 
+  return (numbers.test(passwordToCheck))
 }
 
+export function testLowercase (passwordToCheck) { 
+  return (lowercase.test(passwordToCheck))
+}
 
+export function testUppercase (passwordToCheck) { 
+  return (uppercase.test(passwordToCheck))
+}
+
+export function testSpecial (passwordToCheck) { 
+  return (special.test(passwordToCheck))
+}
+
+export function testLength (passwordToCheck) { 
+  return (length.test(passwordToCheck))
+}
+
+export function testOverall (passwordToCheck) { 
+  return (fullregex.test(passwordToCheck))
+}
