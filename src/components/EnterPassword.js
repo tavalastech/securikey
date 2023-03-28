9ea8242b0689e243b153fc2b12014a3c9850e0a9
USFSeniorProject2023 <124305231+USFSeniorProject2023@users.noreply.github.com>
2023-03-28 12:55:57 -0400

    PKI changes

--- src/components/EnterPassword.js
+++ src/components/EnterPassword.js
@@ -17,16 +17,53 @@ import {
   testSpecial,
   testLength,
   testOverall,
+  containsAddress,
+  containsBirthday,
+  containsFavoriteNumber,
+  containsFirstName,
+  containsHoliday,
+  containsHometown,
+  containsKidsBirthdays,
+  containsKidsFirstNames,
+  containsKidsLastNames,
+  containsLastName,
+  containsMiddleName,
+  containsParentFirstNames,
+  containsParentLastNames,
+  containsPetName,
+  containsSignificantDates, 
+  containsSportsTeams,
+  containsSpouseFirstName,
+  containsSpouseLastName,
+  strengthScore
 } from "../functions/backend"; // Import Backend Functions
 
 
 
-// This is where we write the EnterPassword component function
+// This is where we write the EnterPassword component
 
 export default function EnterPassword() {
-
   // useRef is a React Hook that allows the user to reference the input function
   const passwordRef = useRef();
+  const addressRef = useRef();
+  const birthdayRef = useRef();
+  const favoriteNumberRef = useRef();
+  const firstNameRef = useRef();
+  const holidayRef = useRef();
+  const hometownRef = useRef();
+  const kidsBirthdaysRef = useRef();
+  const kidsFirstNamesRef = useRef();
+  const kidsLastNamesRef = useRef();
+  const lastNameRef = useRef();
+  const middleNameRef = useRef();
+  const parentFirstNamesRef = useRef();
+  const parentLastNamesRef = useRef();
+  const petNameRef = useRef();
+  const significantDatesRef = useRef();
+  const sportsTeamsRef = useRef();
+  const spouseFirstNameRef = useRef(); 
+  const spouseLastNameRef = useRef();
+
 
   /* State Variables
       
@@ -60,6 +97,25 @@ export default function EnterPassword() {
   const [special, setSpecial] = useState("");
   const [length, setLength] = useState("");
   const [overall, setOverall] = useState("");
+  const [address, setAddress] = useState("");
+  const [birthday, setBirthday] = useState("");
+  const [favoriteNumber, setFavoriteNumber] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [middleName, setMiddleName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [kidsLastNames, setKidsLastNames] = useState("");
+  const [kidsFirstNames, setKidsFirstNames] = useState("");
+  const [kidsBirthdays, setKidsBirthdays] = useState("");
+  const [holiday, setHoliday] = useState("");
+  const [hometown, setHometown] = useState("");
+  const [parentFirstNames, setParentFirstNames] = useState("");
+  const [parentLastNames, setParentLastNames] = useState("");
+  const [petName, setPetName] = useState("");
+  const [significantDates, setSignificantDates] = useState("");
+  const [sportsTeams, setSportsTeams] = useState("");
+  const [spouseFirstName, setSpouseFirstName] = useState("");
+  const [spouseLastName, setSpouseLastName] = useState("");
+
 
   /*  On Submit Function 
   
@@ -70,9 +126,28 @@ export default function EnterPassword() {
 
   */
 
-  // Function gras password, test against password, and assigns result to state Variable
+  // Functions To Password and Assigns To State Variable
   function onSubmit(e) {
     const password = passwordRef.current.value;
+    const address = addressRef.current.value;
+    const birthday = birthdayRef.current.value;
+    const favoriteNumber = favoriteNumberRef.current.value;
+    const firstName = firstNameRef.current.value;
+    const middleName = middleNameRef.current.value;
+    const lastName = lastNameRef.current.value;
+    const kidsLastName = kidsLastNamesRef.current.value;
+    const kidsFirstNames = kidsFirstNamesRef.current.value;
+    const kidsBirthdays = kidsBirthdaysRef.current.value;
+    const holiday = holidayRef.current.value;
+    const hometown = hometownRef.current.value;
+    const parentFirstNames = parentFirstNamesRef.current.value;
+    const parentLastNames = parentLastNamesRef.current.value;
+    const petName = petNameRef.current.value;
+    const significantDates = significantDatesRef.current.value;
+    const sportsTeams = sportsTeamsRef.current.value;
+    const spouseFirstName = spouseFirstNameRef.current.value;
+    const spouseLastName = spouseLastNameRef.current.value;
+
     if (password === "") return;
     setNumber(testNumbers(password) + "");
     setLowercase(testLowercase(password) + "");
@@ -80,14 +155,53 @@ export default function EnterPassword() {
     setSpecial(testSpecial(password) + "");
     setLength(testLength(password) + "");
     setOverall(testOverall(password) + "");
+    setAddress(containsAddress(password, address) + "");
+    setBirthday(containsBirthday(password, birthday ) + "");
+    setFavoriteNumber(containsFavoriteNumber(password, favoriteNumber) + "");
+    setFirstName(containsFirstName(password, firstName) + "");
+    setHoliday(containsHoliday(password, holiday) + "");
+    setHometown(containsHometown(password, hometown) + "");
+    setKidsBirthdays(containsKidsBirthdays(password, kidsBirthdays) + "");
+    setKidsFirstNames(containsKidsFirstNames(password, kidsFirstNames) + "");
+    setKidsLastNames(containsKidsLastNames(password, kidsLastName) + "");
+    setLastName(containsLastName(password, lastName) + "");
+    setMiddleName(containsMiddleName(password, middleName) + "");
+    setParentFirstNames(containsParentFirstNames(password, parentFirstNames) + "");
+    setParentLastNames(containsParentLastNames(password, parentLastNames) + "");
+    setPetName(containsPetName(password, petName) + "");
+    setSignificantDates(containsSignificantDates(password, significantDates) + "");
+    setSportsTeams(containsSportsTeams(password, sportsTeams) + "");
+    setSpouseFirstName(containsSpouseFirstName(password, spouseFirstName) + "");
+    setSpouseLastName(containsSpouseLastName(password, spouseLastName) + "");
     setIsShown((current) => !current);
+  
   }
 
-  // Returned JSX component
+
+  // Returns HTML component
 
   return (
     <>
+      <input ref={addressRef} type="text" />
+      <input ref={firstNameRef} type="text" />
+      <input ref={lastNameRef} type="text" />
+      <input ref={middleNameRef} type="text" />
+      <input ref={birthdayRef} type="text" />
+      <input ref={favoriteNumberRef} type="text" />
+      <input ref={significantDatesRef} type="text" />
+      <input ref={holidayRef} type="text" />
+      <input ref={sportsTeamsRef} type="text" />
+      <input ref={hometownRef} type="text" />
+      <input ref={petNameRef} type="text" />
+      <input ref={spouseFirstNameRef} type="text" />
+      <input ref={spouseLastNameRef} type="text" />
+      <input ref={parentFirstNamesRef} type="text" />
+      <input ref={parentLastNamesRef} type="text" />
+      <input ref={kidsFirstNamesRef} type="text" />
+      <input ref={kidsLastNamesRef} type="text" />
+      <input ref={kidsBirthdaysRef} type="text" />
       <input ref={passwordRef} type="text" />
+      
       <button class="submitButton" onClick={onSubmit}>
         Submit
       </button>
@@ -103,8 +217,30 @@ export default function EnterPassword() {
             <div>Does it contain a uppercase letter: {uppercase}</div>
             <div>Does it contain a special character: {special}</div>
             <div>Is it atleast 8 characters long: {length}</div>
+            <div>Does it contain my address: {address}</div>
+            <div>Does it contain my first name: {firstName}</div>
+            <div>Does it contain my last name: {lastName}</div>
+            <div>Does it contain my middle name: {middleName}</div>
+            <div>Does it contain my birthday: {birthday}</div>
+            <div>Does it contain my favorite number: {favoriteNumber}</div>
+            <div>Does it contain my significant dates: {significantDates}</div>
+            <div>Does it contain my favorite holiday: {holiday}</div>
+            <div>Does it contain my favorite sports teams: {sportsTeams}</div>
+            <div>Does it contain my hometown: {hometown}</div>
+            <div>Does it contain my pet's name: {petName}</div>
+            <div>Does it contain my spouse's first name: {spouseFirstName}</div>
+            <div>Does it contain my spouse's last name: {spouseLastName}</div>
+            <div>Does it contain my parents' first names: {parentFirstNames}</div>
+            <div>Does it contain my parents' last names: {parentLastNames}</div>
+            <div>Does it contain my kids' first names: {kidsFirstNames}</div>
+            <div>Does it contain my kids' last names: {kidsLastNames}</div>
+            <div>Does it contain my kids' birthdays: {kidsBirthdays}</div>
+            
+            
           </section>
+          <h2>Does This Password Pass The Test? {overall.toUpperCase()}</h2>
           <h2>Does This Password Pass The Test: {overall}</h2>
+          
         </div>
       )}
     </>
--- src/functions/backend.js
+++ src/functions/backend.js
@@ -1,37 +1,260 @@
-/* These are the variables that store the REGEX string 
+/* These are the variables that store the REGEX passwordToChecking 
    that test the password input */
-const fullregex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])^(?=.*["'()*,-./:;<>?_`~@#$%^&+=!\][}{|\\ ]).{8,}$/;
-const numbers = /^(?=.*\d)/;
-const lowercase = /^(?=.*[a-z])/;
-const uppercase = /^(?=.*[A-Z])/;
-const special = /^(?=.*["'()*,-./:;<>?_`~@#$%^&+=!\][}{|\\ ])/;
-const length = /.{8,}$/;
-
-/* These are the function that test the password input and 
-   returns true or false */ 
-
-export function testNumbers (passwordToCheck) { 
-  return (numbers.test(passwordToCheck))
-}
-
-export function testLowercase (passwordToCheck) { 
-  return (lowercase.test(passwordToCheck))
-}
-
-export function testUppercase (passwordToCheck) { 
-  return (uppercase.test(passwordToCheck))
-}
-
-export function testSpecial (passwordToCheck) { 
-  return (special.test(passwordToCheck))
-}
-
-export function testLength (passwordToCheck) { 
-  return (length.test(passwordToCheck))
-}
-
-export function testOverall (passwordToCheck) { 
-  return (fullregex.test(passwordToCheck))
-}
-
-//To Fix Tk's Fuckup 
+   const fullregex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])^(?=.*["'()*,-./:;<>?_`~@#$%^&+=!\][}{|\\ ]).{8,}$/;
+   const numbers = /^(?=.*\d)/;
+   const lowercase = /^(?=.*[a-z])/;
+   const uppercase = /^(?=.*[A-Z])/;
+   const special = /^(?=.*["'()*,-./:;<>?_`~@#$%^&+=!\][}{|\\ ])/;
+   const length = /.{8,}$/;
+   
+   /* These are the function that test the password input and 
+      returns true or false */ 
+   
+   let count = 0;
+   let totalCount = 0;
+   let totalStrengthScore = 0;
+   
+   export function testNumbers (passwordToCheck) { 
+     return (numbers.test(passwordToCheck))
+   }
+   
+   export function testLowercase (passwordToCheck) { 
+     return (lowercase.test(passwordToCheck))
+   }
+   
+   export function testUppercase (passwordToCheck) { 
+     return (uppercase.test(passwordToCheck))
+   }
+   
+   export function testSpecial (passwordToCheck) { 
+     return (special.test(passwordToCheck))
+   }
+   
+   export function testLength (passwordToCheck) { 
+     return (length.test(passwordToCheck))
+   }
+   
+   export function testOverall (passwordToCheck) { 
+     return (fullregex.test(passwordToCheck))
+   }
+   
+   export function containsFirstName(passwordToCheck, pkiValueEntered) {
+     if (passwordToCheck.includes(pkiValueEntered)) {
+         totalCount++;
+         return true;
+     }
+     else {
+         count++;
+         totalCount++;
+         return false;
+         }
+   }
+   
+   export function containsMiddleName(passwordToCheck, pkiValueEntered) {
+     if (passwordToCheck.includes(pkiValueEntered)) {
+         totalCount++;
+         return true;
+     }
+     else {
+         count++;
+         totalCount++;
+         return false;
+         }
+   }
+   
+   export function containsLastName(passwordToCheck, pkiValueEntered) {
+     if (passwordToCheck.includes(pkiValueEntered)) {
+         totalCount++;
+         return true;
+     }
+     else {
+         count++;
+         totalCount++;
+         return false;
+         }
+   }
+   
+   export function containsPetName(passwordToCheck, pkiValueEntered) {
+     if (passwordToCheck.includes(pkiValueEntered)) {
+         totalCount++;
+         return true;
+     }
+     else {
+         count++;
+         totalCount++;
+         return false;
+         }
+   }
+   
+   export function containsParentFirstNames(passwordToCheck, pkiValueEntered) {
+     if (passwordToCheck.includes(pkiValueEntered)) {
+         totalCount++;
+         return true;
+     }
+     else {
+         count++;
+         totalCount++;
+         return false;
+         }
+   }
+   
+   export function containsParentLastNames(passwordToCheck, pkiValueEntered) {
+     if (passwordToCheck.includes(pkiValueEntered)) {
+         totalCount++;
+         return true;
+     }
+     else {
+         count++;
+         totalCount++;
+         return false;
+         }
+   }
+   
+   export function containsSpouseFirstName(passwordToCheck, pkiValueEntered) {
+     if (passwordToCheck.includes(pkiValueEntered)) {
+         totalCount++;
+         return true;
+     }
+     else {
+         count++;
+         totalCount++;
+         return false;
+         }
+   }
+   
+   export function containsSpouseLastName(passwordToCheck, pkiValueEntered) {
+     if (passwordToCheck.includes(pkiValueEntered)) {
+         totalCount++;
+         return true;
+     }
+     else {
+         count++;
+         totalCount++;
+         return false;
+         }
+   }
+   
+   export function containsKidsFirstNames(passwordToCheck, pkiValueEntered) {
+     if (passwordToCheck.includes(pkiValueEntered)) {
+         totalCount++;
+         return true;
+     }
+     else {
+         count++;
+         totalCount++;
+         return false;
+         }
+   }
+   
+   export function containsKidsLastNames(passwordToCheck, pkiValueEntered) {
+     if (passwordToCheck.includes(pkiValueEntered)) {
+         totalCount++;
+         return true;
+     }
+     else {
+         count++;
+         totalCount++;
+         return false;
+         }
+   }
+   
+   export function containsBirthday(passwordToCheck, pkiValueEntered) {
+     if (passwordToCheck.includes(pkiValueEntered)) {
+         totalCount++;
+         return true;
+     }
+     else {
+         count++;
+         totalCount++;
+         return false;
+         }
+   }
+   
+   export function containsKidsBirthdays(passwordToCheck, pkiValueEntered) {
+     if (passwordToCheck.includes(pkiValueEntered)) {
+         totalCount++;
+         return true;
+     }
+     else {
+         count++;
+         totalCount++;
+         return false;
+         }
+   }
+   
+   export function containsHometown(passwordToCheck, pkiValueEntered) {
+     if (passwordToCheck.includes(pkiValueEntered)) {
+         totalCount++;
+         return true;
+     }
+     else {
+         count++;
+         totalCount++;
+         return false;
+         }
+   }
+   
+   export function containsSportsTeams(passwordToCheck, pkiValueEntered) {
+     if (passwordToCheck.includes(pkiValueEntered)) {
+         totalCount++;
+         return true;
+     }
+     else {
+         count++;
+         totalCount++;
+         return false;
+         }
+   }
+   
+   export function containsHoliday(passwordToCheck, pkiValueEntered) {
+     if (passwordToCheck.includes(pkiValueEntered)) {
+         totalCount++;
+         return true;
+     }
+     else {
+         count++;
+         totalCount++;
+         return false;
+         }
+   }
+   
+   export function containsSignificantDates(passwordToCheck, pkiValueEntered) {
+     if (passwordToCheck.includes(pkiValueEntered)) {
+         totalCount++;
+         return true;
+     }
+     else {
+         count++;
+         totalCount++;
+         return false;
+         }
+   }
+   
+   export function containsFavoriteNumber(passwordToCheck, pkiValueEntered) {
+     if (passwordToCheck.includes(pkiValueEntered)) {
+         totalCount++;
+         return true;
+     }
+     else {
+         count++;
+         totalCount++;
+         return false;
+         }
+   }
+   
+   export function containsAddress(passwordToCheck, pkiValueEntered) {
+     if (passwordToCheck.includes(pkiValueEntered)) {
+         totalCount++;
+         return true;
+     }
+     else {
+       count++;
+       totalCount++;
+       return false;
+       }
+   }
+   
+   export function strengthScore(count, totalCount){
+     totalStrengthScore = count/totalCount;
+     return totalStrengthScore;
+   }
